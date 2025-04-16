1533c70b8fd83ee083b2d2ad2039f91510033ff2
Kalleo Leandro dos Santos Leal <kalleo.leal@gmail.com>
2025-04-15 23:09:52 -0300

    Configuracao Docker, inicializacao db

--- db/init.sql
+++ db/init.sql
@@ -1,2 +1,10 @@
 create database sistema_vendas;
 use sistema_vendas;
+
+create table usuarios(
+    id int primary key auto_increment,
+    nome varchar(255) not null,
+    login varchar(64) not null,
+    senha varchar(16) not null
+    perfil int not null
+);
